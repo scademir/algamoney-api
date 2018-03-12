8033ed4538dd04b2454ef345c9048f35285e958c
Ademir Santos <scademir@gmail.com>
2018-03-12 08:12:24 -0300

    Adicionando Categoria

---
+++ src/main/java/com/algamoney/api/resource/CategoriaResource.java
@@ -0,0 +1,24 @@
+package com.algamoney.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algamoney.api.model.Categoria;
+import com.algamoney.api.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		return categoriaRepository.findAll();
+	}
+}
